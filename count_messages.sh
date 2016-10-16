642af4267ede022bfa7daab890112f9e62358e5f
lintool <jimmylin@uwaterloo.ca>
2016-10-16 13:55:29 -0400

    Some folder names do have numbers.

--- count_messages.sh
+++ count_messages.sh
@@ -4,7 +4,7 @@ shopt -s nullglob
 
 # http://stackoverflow.com/questions/15216370/how-to-count-number-of-files-in-each-directory
 find maildir -type d -print0 | while read -d '' -r dir; do
-  files=("$dir"/[0-9]*)
+  files=("$dir"/[0-9]*\.)
   printf "%d messages: %s\n" "${#files[@]}" "$dir"
 done
 
